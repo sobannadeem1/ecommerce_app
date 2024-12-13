e191cbb84a8650d4afebc35ce4f2066d7df16be9
Soban Nadeem <soban312004@gmail.com>
2024-12-13 18:04:16 +0500

    frontend

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
-
-const API = axios.create({ baseURL: BASE_URL, withCredentials: true });
+const API = axios.create({
+  baseURL: "https://ecommerce-app-ten-omega.vercel.app/",
+});
 
 export default API;
