e0a6f42dbd0f609397d607abec15a3b096560975
Soban Nadeem <soban312004@gmail.com>
2024-12-13 17:57:19 +0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,14 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const cookieParser = require("cookie-parser");
 const { notFound, errorHandler } = require("./middleware/errorHandler");
+
 connectDB();
 
 const app = express();
 
 app.use(
   cors({
-    origin: process.env.BASE_URL,
+    origin: "http://localhost:3000", // Update this with your frontend's origin
     credentials: true,
     methods: "PUT,POST,GET,DELETE,PATCH,HEAD",
   })
@@ -20,11 +21,18 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use("/api/user/", userRoutes);
 
-app.use(notFound);
-app.use(errorHandler);
+// Define the home route
+app.get("/", (req, res) => {
+  res.send("Welcome to shopping cart API!");
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+// Define the port
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
