f43f45e7a263fd758ce1a2b2815739ec2e5c6ebe
Soban Nadeem <soban312004@gmail.com>
2024-12-14 11:11:05 +0500

    rebder

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,13 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const cookieParser = require("cookie-parser");
 const { notFound, errorHandler } = require("./middleware/errorHandler");
-
 connectDB();
 
 const app = express();
 
 app.use(
   cors({
-    origin: "hhttps://ecommerce-app-b1p3.vercel.app/", // Update this with your frontend's origin
+    origin: "",
     credentials: true,
     methods: "PUT,POST,GET,DELETE,PATCH,HEAD",
   })
@@ -21,18 +20,11 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use("/api/user/", userRoutes);
 
-// Define the home route
-app.get("/", (req, res) => {
-  res.send("Welcome to shopping cart API!");
-});
+app.use(notFound);
+app.use(errorHandler);
 
-// Define the port
-const PORT = process.env.PORT || 3000;
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
