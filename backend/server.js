4575e3d80a9206b2b203f4d16275b679a218a3b3
Soban Nadeem <soban312004@gmail.com>
2024-12-14 13:10:11 +0500

    mkml

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/user/", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
